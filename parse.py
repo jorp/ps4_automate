95117b3a334d4accc446ea6f3e8145bbec4327fa
jorp <jpisaniello@pm.me>
2020-11-25 15:57:44 -0500

    added automatic window seeking.. really ugly for now

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@
 import os
 import re
 import sys
+import subprocess
 
 cmd_dict = {
         "jshort":"xdotool_key",
@@ -38,8 +39,7 @@ def look_up_keycode(key):
     print(cmd_dict.get(key))
     #return cmd_dict.get(key)
 def run_commands(button, repeat = "1"):
-    #print("xdotool key --repeat {} --delay 1000 '{}'".format(repeat, button))
-    os.system("xdotool key --repeat {} --delay 100 '{}'".format(repeat, cmd_dict.get(button)))
+    os.system("xdotool key --repeat {} --delay 1000 '{}'".format(repeat, cmd_dict.get(button)))
     print(cmd_dict.get(button))
 
 with open(sys.argv[1], 'r') as in_file:
@@ -54,8 +54,9 @@ with open(sys.argv[1], 'r') as in_file:
             commands.append(line.strip())
 
 #print(commands)
-os.system("xdotool windowactivate 142606430")
-os.system("xdotool windowfocus 142606430")
+window_id = subprocess.Popen("wmctrl -l | grep 'Chiaki | Stream' | grep -v 'grep' | grep -Eo '0x[0-9a-f]+'", shell=True, stdout=subprocess.PIPE, universal_newlines=True).communicate()[0].strip()
+os.system("xdotool windowactivate {}".format(window_id))
+os.system("xdotool windowfocus {}".format(window_id))
 for item in commands:
     if type(item) is tuple:
         repeat = item[0]
